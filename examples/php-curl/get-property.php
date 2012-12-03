ef57e503500228a5c37091300059d2683e6361f4
Pete Lawrence <pete@carltonsoftware.co.uk>
2012-12-03 14:15:34 +0000

    Demonstrate how to convert the response from the API into a PHP object, and then access it's fields

--- examples/php-curl/get-property.php
+++ examples/php-curl/get-property.php
@@ -15,5 +15,17 @@ curl_setopt($ch, CURLOPT_TIMEOUT, '3');
 //Perform the request, and assign the response to $response
 $response = curl_exec($ch);
 
-//Print out the response
-print $response;
+//Convert the response into a json_objecy
+$property = json_decode($response);
+
+?>
+
+<h1><?= $property->name ?></h1>
+<ul>
+    <li><label>Sleeps:</label> <?=$property->accommodates?></li>
+    <li><label>Bedrooms:</label> <?=$property->bedrooms?></li>
+    <li><label>Pets:</label> <?= ($property->pets) ? 'Yes' : 'No' ?></li>
+    <li><label>Price:</label> &pound;<?=$property->brands->SS->pricing->ranges->{date('Y')}->low ?> to &pound;<?=$property->brands->SS->pricing->ranges->{date('Y')}->high ?></li>
+</ul>
+
+<?= $property->brands->SS->description ?>
