494b5e8a2899f26361d8b74efae9c42cb62844b2
Alex Wyett <alex@carltonsoftware.co.uk>
2018-05-02 07:28:28 +0100

    Update ApiClient.php

--- examples/php-curl/helpers/src/tabs/api/client/ApiClient.php
+++ examples/php-curl/helpers/src/tabs/api/client/ApiClient.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Tabs Rest API object.
  *
@@ -7,12 +6,9 @@
  *
  * @category  API_Client
  * @package   Tabs
- * @author    Alex Wyett <alex@wyett.co.uk>
- * @copyright 2013 Carlton Software
  * @license   http://www.php.net/license/3_01.txt  PHP License 3.01
- * @link      http://www.carltonsoftware.co.uk
+ * @link      http://www.tabs-software.co.uk
  */
-
 namespace tabs\api\client;
 
 /**
@@ -28,11 +24,8 @@ namespace tabs\api\client;
  *
  * @category  API_Client
  * @package   Tabs
- * @author    Alex Wyett <alex@wyett.co.uk>
- * @copyright 2013 Carlton Software
  * @license   http://www.php.net/license/3_01.txt  PHP License 3.01
- * @version   Release: 1
- * @link      http://www.carltonsoftware.co.uk
+ * @link      http://www.tabs-software.co.uk
  */
 class ApiClient
 {
@@ -42,21 +35,21 @@ class ApiClient
      * @var ApiClient
      */
     static $api;
-
+    
     /**
      * Client content type
      *
      * @var string
      */
-    protected $contentType = "application/json";
-
+    protected $contentType = 'application/json';
+    
     /**
      * Local route string
      *
      * @var string
      */
     protected $urlRoute = '';
-
+    
     /**
      * Test mode bool.  If set to true, client pops the data param from
      * the provided params array
@@ -64,42 +57,50 @@ class ApiClient
      * @var boolean
      */
     protected $testMode;
-
+    
     /**
      * Static route object
      *
      * @var resource
      */
     protected $resource;
-
+    
     /**
      * The API key to use for HMAC authentication/
      *
      * @var string
      */
     protected $apiKey;
-
+    
     /**
      * The API secret.
      *
      * @var string
      */
     protected $secret;
-
+    
     /**
      * Array of routes requested by this api instance
      *
      * @var array
      */
     public $routes = array();
-
+    
     /**
      * An HMAC object which contains functions to do HMAC authentication.
      *
      * @var tabs\api\client\HMAC
      */
     public $hmacHelper;
-
+    
+    /**
+     * The raw response from the last curl request.  Added so it is possible to
+     * track exact responses from the API while debugging.
+     *
+     * @var string
+     */
+    protected $lastResponse;
+    
     /**
      * Create a new Api Connection for use within the tabs php client
      * api.
@@ -115,10 +116,10 @@ class ApiClient
         $apiKey = '',
         $secret = ''
     ) {
-        self::$api = new ApiClient($apiUrl, $apiKey, $secret);
+        self::$api = new static($apiUrl, $apiKey, $secret);
         return self::$api;
     }
-
+    
     /**
      * Get the api connection
      *
@@ -130,10 +131,9 @@ class ApiClient
         if (!self::$api) {
             throw new ApiException(null, 'No api connection available');
         }
-
         return self::$api;
     }
-
+    
     /**
      * Initialise
      *
@@ -152,29 +152,14 @@ class ApiClient
         if (!function_exists("curl_exec")) {
             throw new \Exception("Could not find curl_exec, is CURL installed?");
         }
-
         // Setup required fields
         $this->setUrlRoute($urlRoute);
         $this->setApiKey($apiKey);
         $this->setSecret($secret);
         $this->hmacHelper = new \tabs\api\client\Hmac();
     }
-
-
+    
     // ------------------ Public Functions ----------------------//
-
-    /**
-     * Sets the api url
-     *
-     * @param string $urlRoute Route of the API
-     *
-     * @return void
-     */
-    public function setUrlRoute($urlRoute)
-    {
-        $this->urlRoute = trim($urlRoute, "/");
-    }
-
     /**
      * Sets the API key.
      *
@@ -186,7 +171,31 @@ class ApiClient
     {
         $this->apiKey = $apiKey;
     }
-
+    
+    /**
+     * Sets the api url
+     *
+     * @param string $urlRoute Route of the API
+     *
+     * @return void
+     */
+    public function setUrlRoute($urlRoute)
+    {
+        $this->urlRoute = trim($urlRoute, "/");
+    }
+    
+    /**
+     * Sets the last curl response
+     *
+     * @param string $lastResponse Last curl response
+     *
+     * @return void
+     */
+    public function setLastResponse($lastResponse)
+    {
+        $this->lastResponse = $lastResponse;
+    }
+    
     /**
      * Sets the API secret.
      *
@@ -198,27 +207,7 @@ class ApiClient
     {
         $this->secret = $secret;
     }
-
-    /**
-     * Gets the API secret.
-     *
-     * @return string The API secret.
-     */
-    public function getSecret()
-    {
-        return $this->secret;
-    }
-
-    /**
-     * Gets the API key.
-     *
-     * @return string The API key that is set for this client.
-     */
-    public function getApiKey()
-    {
-        return $this->apiKey;
-    }
-
+    
     /**
      * Sets the api test mode
      *
@@ -230,7 +219,27 @@ class ApiClient
     {
         $this->testMode = $bool;
     }
-
+    
+    /**
+     * Gets the API secret.
+     *
+     * @return string The API secret.
+     */
+    public function getSecret()
+    {
+        return $this->secret;
+    }
+    
+    /**
+     * Gets the API key.
+     *
+     * @return string The API key that is set for this client.
+     */
+    public function getApiKey()
+    {
+        return $this->apiKey;
+    }
+    
     /**
      * Returns the url of the api
      *
@@ -240,7 +249,7 @@ class ApiClient
     {
         return $this->urlRoute;
     }
-
+    
     /**
      * Returns all of the urls that have been accessed in the api instance
      *
@@ -250,7 +259,7 @@ class ApiClient
     {
         return $this->routes;
     }
-
+    
     /**
      * Multiple get requests
      *
@@ -263,7 +272,7 @@ class ApiClient
     {
         return $this->_doMultiRequest('_get', $paths);
     }
-
+    
     /**
      * Get API call
      *
@@ -276,7 +285,7 @@ class ApiClient
     {
         return $this->_doRequest("_get", $urlPath, $params);
     }
-
+    
     /**
      * Post API call
      *
@@ -289,7 +298,7 @@ class ApiClient
     {
         return $this->_doRequest("_post", $urlPath, $params);
     }
-
+    
     /**
      * Put API call
      *
@@ -302,7 +311,7 @@ class ApiClient
     {
         return $this->_doRequest("_put", $urlPath, $params);
     }
-
+    
     /**
      * Delete API call
      *
@@ -315,7 +324,7 @@ class ApiClient
     {
         return $this->_doRequest("_delete", $urlPath, $params);
     }
-
+    
     /**
      * Options API call
      *
@@ -328,7 +337,7 @@ class ApiClient
     {
         return $this->_doRequest("_options", $urlPath, $params);
     }
-
+    
     /**
      * Return the url parameters with the included hmac hash
      *
@@ -348,7 +357,7 @@ class ApiClient
             return $params;
         }
     }
-
+    
     /**
      * Return the url parameters with the included hmac hash in query string
      * format.
@@ -363,11 +372,18 @@ class ApiClient
             $this->getHmacParams($params)
         );
     }
-
-
+    
+    /**
+     * Fetch the last curl response
+     *
+     * @return string
+     */
+    public function getLastResponse()
+    {
+        return $this->lastResponse;
+    }
+    
     // ------------------ Private Functions ---------------------//
-
-
     /**
      * Function used to call the api functions, _get, _post, _put & _delete
      * multiple times!
@@ -385,7 +401,6 @@ class ApiClient
                 'Number of multi connections must not exceed 20'
             );
         }
-
         $mResource = curl_multi_init();
         $resources = array();
         $results = array();
@@ -399,13 +414,11 @@ class ApiClient
             curl_multi_add_handle($mResource, $this->resource);
             curl_close($this->resource);
         }
-
         // execute the handles
         $running = null;
         do {
             curl_multi_exec($mResource, $running);
         } while ($running > 0);
-
         foreach ($resources as $res) {
             extract(
                 $this->_getResponseData(
@@ -422,11 +435,10 @@ class ApiClient
             );
             curl_multi_remove_handle($mResource, $res);
         }
-
         curl_multi_close($mResource);
         return $results;
     }
-
+    
     /**
      * Function used to call the api functions, _get, _post, _put & _delete
      *
@@ -436,24 +448,19 @@ class ApiClient
      *
      * @return boolean or result
      */
-    private function _doRequest($apiFunc, $urlPath, $params)
+    protected function _doRequest($apiFunc, $urlPath, $params)
     {
         if (method_exists($this, $apiFunc)) {
             // Get parameters with hmac hash
             $params = $this->getHmacParams($params);
-
             // Request curl response
             $this->$apiFunc($urlPath, $params);
-
             // Grab response
             $response = $this->_curlExec();
-
             // Return $header, $body, $location etc from new request
             extract($this->_getResponseData($response));
-
             // Close current request
             curl_close($this->resource);
-
             // Attempt to output response.
             // All variables apart from $headers are returned from the
             // extract call on the _getResponseData function
@@ -468,7 +475,7 @@ class ApiClient
             return false;
         }
     }
-
+    
     /**
      * Sets the options for the curl request, also adds the content type header
      *
@@ -482,21 +489,18 @@ class ApiClient
         // Include headers in response string
         curl_setopt($this->resource, CURLOPT_HEADER, 1);
         curl_setopt($this->resource, CURLOPT_VERBOSE, 0);
-
         // return the output into a variable
         curl_setopt($this->resource, CURLOPT_RETURNTRANSFER, 1);
-
         // Set curl resource to follow redirects
         curl_setopt($this->resource, CURLOPT_FOLLOWLOCATION, $follow);
-
         // Set the User-Agent header
         curl_setopt(
             $this->resource,
             CURLOPT_USERAGENT,
-            'TABS ApiClient (http://github.com/CarltonSoftware/tocc-api-client)'
+            'TABS ApiClient (http://github.com/CarltonSoftware/tabs-api-client)'
         );
     }
-
+    
     /**
      * Completes the curl request, also adds the content type header
      *
@@ -509,11 +513,11 @@ class ApiClient
     {
         // Set the curl options
         $this->_setCurlOpt($follow);
-
         // Commit the curl request and return the response
-        return curl_exec($this->resource);
+        $this->setLastResponse(curl_exec($this->resource));
+        return $this->getLastResponse();
     }
-
+    
     /**
      * Return a array of data about the previous response
      *
@@ -529,17 +533,13 @@ class ApiClient
         }
         // Extract HTTP status code and header size
         extract($this->_getHeaderAndStatus($resource));
-
         // Headers String
         $header = substr($response, 0, $headerSize);
-
         // Response Body
         $body = substr($response, $headerSize);
-
         // Get Location if redirect required
         preg_match('/Location:(.*?)\n/i', $header, $matches);
         $location = trim(array_pop($matches));
-
         // Get headers and put them into a key => value array
         $headers = array();
         $headerLines = array_filter(explode("\r\n", $header));
@@ -549,7 +549,6 @@ class ApiClient
                 $headers[trim($parts[0])] = trim($parts[1]);
             }
         }
-
         return array(
             "statusCode" => $statusCode,
             "headerSize" => $headerSize,
@@ -560,7 +559,7 @@ class ApiClient
             "location"   => $location
         );
     }
-
+    
     /**
      * Return the header and status in an array from a specified resource
      *
@@ -576,8 +575,7 @@ class ApiClient
             'info'       => curl_getinfo($resource)
         );
     }
-
-
+    
     /**
      * Function use to return the response in the correct format given the
      * content type
@@ -595,12 +593,10 @@ class ApiClient
         // Continue to output
         $originalBody = $body;
         $body = json_decode($body);
-
         // Set null value to empty string
         if ($body === null) {
             $body = '';
         }
-
         return (object) array(
             "status" => $statusCode,
             "response" => $body,
@@ -610,8 +606,7 @@ class ApiClient
             "body" => $originalBody
         );
     }
-
-
+    
     /**
      * Handles a post request to the api
      *
@@ -626,56 +621,10 @@ class ApiClient
         if (count($params) > 0) {
             $urlPath .= "?" . $this->_getHttpQuery($params);
         }
-
         // Find the curl path to use
         $this->_getResource($urlPath);
     }
-
-    /**
-     * Handles a post request to the api
-     *
-     * @param string $urlPath The path being requested
-     * @param array  $params  An array of parameters to be sent
-     *
-     * @return string
-     */
-    private function _post($urlPath, $params)
-    {
-        // Find the curl path to use
-        $this->_getResource($urlPath);
-
-        // use POST
-        curl_setopt($this->resource, CURLOPT_POST, 1);
-
-        // Pop data param into pure json as mock server cannot handle parameter
-        // arguments
-        if ($this->testMode) {
-            if (isset($params['data'])) {
-                $params = $params['data'];
-            }
-        }
-
-        // Set the post fields.  Requires the function
-        // http_build_query to encode the parameters
-        curl_setopt(
-            $this->resource,
-            CURLOPT_POSTFIELDS,
-            is_array($params) ? $this->_getHttpQuery($params) : $params
-        );
-
-        //Set the header
-        curl_setopt(
-            $this->resource,
-            CURLOPT_HTTPHEADER,
-            array(
-                'Content-Type: application/x-www-form-urlencoded'
-            )
-        );
-
-        // Set the content type
-        $this->contentType = "application/x-www-form-urlencoded;charset=UTF-8";
-    }
-
+    
     /**
      * Handles a put request to the api
      *
@@ -686,12 +635,6 @@ class ApiClient
      */
     private function _put($urlPath, $params)
     {
-        // Find the curl path to use
-        $this->_getResource($urlPath);
-
-        // Set custom header
-        curl_setopt($this->resource, CURLOPT_CUSTOMREQUEST, "PUT");
-
         // Pop data param into pure json as mock server cannot handle parameter
         // arguments
         if ($this->testMode) {
@@ -699,7 +642,18 @@ class ApiClient
                 $params = $params['data'];
             }
         }
-
+        
+        // Add the API key as a query string part
+        if ($params['APIKEY']) {
+            $urlPath .= "?APIKEY=" . $params['APIKEY'];
+            unset($params['APIKEY']);
+        }
+        
+        // Find the curl path to use
+        $this->_getResource($urlPath);
+        // Set custom header
+        curl_setopt($this->resource, CURLOPT_CUSTOMREQUEST, "PUT");
+        
         // Set the post fields.  Requires the function
         // http_build_query to encode the parameters
         curl_setopt(
@@ -707,7 +661,7 @@ class ApiClient
             CURLOPT_POSTFIELDS,
             is_array($params) ? $this->_getHttpQuery($params) : $params
         );
-
+        
         //Set the header
         curl_setopt(
             $this->resource,
@@ -716,11 +670,26 @@ class ApiClient
                 'Content-Type: application/x-www-form-urlencoded'
             )
         );
-
+        
         // Set the content type
         $this->contentType = "application/x-www-form-urlencoded;charset=UTF-8";
     }
-
+    
+    /**
+     * Handles a post request to the api
+     *
+     * @param string $urlPath The path being requested
+     * @param array  $params  An array of parameters to be sent
+     *
+     * @return string
+     */
+    private function _post($urlPath, $params)
+    {
+        $this->_put($urlPath, $params);
+        curl_setopt($this->resource, CURLOPT_POST, 1);
+        curl_setopt($this->resource, CURLOPT_CUSTOMREQUEST, "POST");
+    }
+    
     /**
      * Handles a delete request to the api
      *
@@ -735,14 +704,12 @@ class ApiClient
         if (count($params) > 0) {
             $urlPath .= "?" . $this->_getHttpQuery($params);
         }
-
         // Find the curl path to use
         $this->_getResource($urlPath);
-
         // Set custom header
         curl_setopt($this->resource, CURLOPT_CUSTOMREQUEST, "DELETE");
     }
-
+    
     /**
      * Handles a delete request to the api
      *
@@ -757,14 +724,12 @@ class ApiClient
         if (count($params) > 0) {
             $urlPath .= "?" . $this->_getHttpQuery($params);
         }
-
         // Find the curl path to use
         $this->_getResource($urlPath);
-
         // Set custom header
         curl_setopt($this->resource, CURLOPT_CUSTOMREQUEST, "OPTIONS");
     }
-
+    
     /**
      * Function used to setup the static curl resource
      *
@@ -779,19 +744,17 @@ class ApiClient
             // Remove leading and trailing slashes
             $urlPath = $this->urlRoute . "/" . trim($urlPath, "/");
         }
-
         // Add to route history
         array_push($this->routes, $urlPath);
-
         // Get resource
         $this->resource = curl_init($urlPath);
     }
     
     /**
      * Encode an array of parameters into a string
-     * 
+     *
      * @param array $params Parameters to encode
-     * 
+     *
      * @return string
      */
     private function _getHttpQuery($params)
